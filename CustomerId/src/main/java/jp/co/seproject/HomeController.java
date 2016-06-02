311c5236676112963116a38ba216cb949e3ccf2a
inageryuji <inageryuji@se-project.co.jp>
2016-06-02 15:55:26 +0900

    画面を実装（仮）

--- CustomerId/src/main/java/jp/co/seproject/HomeController.java
+++ CustomerId/src/main/java/jp/co/seproject/HomeController.java
@@ -1,39 +1,65 @@
 package jp.co.seproject;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+@Autowired
+private  SystemDao  SystemDaoInpl;
+
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+    public String home(Model model) {
+        return "home";
+    }
+
+	@RequestMapping(value = "/top", method = RequestMethod.GET)
+	public String top(Model model) {
+		return "top";
 	}
-	
+
+	@RequestMapping(value = "/entry", method = RequestMethod.GET)
+    public String getentry(@ModelAttribute CustomerData cutomerData, Model model) {
+	    CustomerData cd = new CustomerData();
+        model.addAttribute("cutomerData", cd);
+        return "entry";
+    }
+	@RequestMapping(value = "/entry", method = RequestMethod.POST)
+    public String postentry(@ModelAttribute CustomerData cd, Model model) {
+            String name = cd.getName().trim();
+            String address = cd.getAddress().trim();
+            String tel = cd.getTel().trim();
+            String mailaddress = cd.getMailaddress().trim();
+            String gender = cd.getGender().trim();
+            int birth = cd.getBirth().trim();
+
+            SystemDao.insert(name, address, tel);
+
+        return "entry";
+    }
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        return "list";
+    }
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+    public String update(Model model) {
+        return "update";
+    }
+	@RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public String detail (Model model) {
+        return "detail";
+    }
+	@RequestMapping(value = "/cardlist", method = RequestMethod.GET)
+    public String cardlist(Model model) {
+        return "cardlist";
+    }
+	@RequestMapping(value = "/mail", method = RequestMethod.GET)
+    public String mail(Model model) {
+        return "mail";
+    }
 }
--- CustomerId/src/main/webapp/WEB-INF/views/home.jsp
+++ CustomerId/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,21 +1,15 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page session="false"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
 <html>
 <head>
-<title>顧客ポイントカード管理システム</title>
-
+<meta http-equiv="Refresh"
+	content="0;URL=/seproject/top">
+<title>Home</title>
 </head>
 <body>
-	<h1 align=center>顧客ポイントカード管理システム</h1>
 
-	<P align=center>
-		<a href="/seproject/entry"><input type=button value="顧客登録"></a>
-		<a href="/seproject/list"><input type=button value="顧客一覧"></a>
-	</P>
-	<p align=center>
-		<a href="/seproject/list"><input type=button value="カード一覧"></a>
-		<a href="seproject/mail"><input type=button value="メール送信"></a>
-	</p>
 </body>
 </html>
